b223d7386edf157b2436e9b7caee7a73de623fd1
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2024-01-24 18:00:31 -0800

    Fixed bug with db location, can use form to make api call

--- models.py
+++ models.py
@@ -1,31 +1,35 @@
 # models.py
+import os
 import sqlite3
 from datetime import datetime
 
+# Get the absolute path of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+# Join the script directory with the database file name
+db_path = os.path.join(script_dir, 'api_cache.db')
+
+
+def db_operation(sql, params=()):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute(sql, params)
+    if sql.startswith('SELECT'):
+        result = c.fetchone()
+    else:
+        conn.commit()
+        result = None
+    conn.close()
+    return result
+
 
 def init_db():
-    print("Initializing database...")
-    conn = sqlite3.connect('api_cache.db')
-    c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS cache (symbol text, timestamp text, data text)")
-    conn.commit()
-    conn.close()
-    print("Database initialized.")
-
+    db_operation("CREATE TABLE IF NOT EXISTS cache (symbol text, timestamp text, data text)")
 
 
 def insert_data(symbol, data):
-    conn = sqlite3.connect('api_cache.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO cache VALUES (?, ?, ?)", (symbol, str(datetime.now()), data))
-    conn.commit()
-    conn.close()
+    db_operation("INSERT INTO cache VALUES (?, ?, ?)", (symbol, str(datetime.now()), data))
 
 
 def get_data_from_db(symbol):
-    conn = sqlite3.connect('api_cache.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM cache WHERE symbol=?", (symbol,))
-    result = c.fetchone()
-    conn.close()
-    return result
+    return db_operation("SELECT * FROM cache WHERE symbol=?", (symbol,))
+
