60144fe5824fc17d30c6664bfac49a91b7ef3fb9
VolcanicMonkey5 <lucasmartiniscool@gmail.com>
2024-02-27 15:52:48 -0800

    Format based on categories

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -17,18 +17,45 @@ function addSpaces(s) {
         .replace(/([0-9])([A-Za-z])/g, '$1 $2');  // Add space between a number and a letter
 }
 
-// Iterate over each key-value pair in the overview data
-for (let key in overviewData) {
-    // Create a new list item
-    let listItem = document.createElement('li');
+// Define the categories
+let categories = {
+    "General Company Info": ["Symbol", "Name", "Industry", "AssetType", "Address", "Description"],
+    "Relevant Financial Data": ["MarketCapitalization", "EBITDA", "PERatio", "PEGRatio", "BookValue", "EPS", "ProfitMargin", "RevenueTTM", "GrossProfitTTM", "SharesOutstanding"],
+    "Trailing 12 Month Info": ["RevenuePerShareTTM", "OperatingMarginTTM", "ReturnOnAssetsTTM", "ReturnOnEquityTTM", "DilutedEPSTTM", "TrailingPE", "PriceToSalesRatioTTM", "52WeekHigh", "52WeekLow"],
+    "Dividends": ["DividendPerShare", "DividendYield", "DividendDate", "ExDividendDate"],
+    "Long-term growth and future value": ["FiscalYearEnd", "LatestQuarter", "QuarterlyEarningsGrowthYOY", "QuarterlyRevenueGrowthYOY", "AnalystTargetPrice", "ForwardPE"],
+    "Misc Financial Data": ["CIK", "Beta", "PriceToBookRatio", "EVToRevenue", "EVToEBITDA", "200DayMovingAverage", "50DayMovingAverage"],
+    "Misc Company Info": ["CIK", "Exchange", "Currency", "Country", "Sector"]
+};
 
-    // Format the key and value
-    let formattedKey = addSpaces(key);
-    let formattedValue = isNaN(overviewData[key]) ? overviewData[key] : numberWithCommas(overviewData[key]);
 
-    // Set the text of the list item to the formatted key and value
-    listItem.innerHTML = `<span class="key">${formattedKey}:</span> <span class="value">${formattedValue}</span>`;
+// Iterate over each category
+for (let categoryName in categories) {
+    // Create a new container for this category
+    let categoryContainer = document.createElement('div');
+    categoryContainer.className = 'category';
+    categoryContainer.innerHTML = `<h4>${categoryName}</h4>`;
 
-    // Add the list item to the sidebar
-    sidebar.appendChild(listItem);
+    // Iterate over each field in this category
+    for (let field of categories[categoryName]) {
+        // Check if this field exists in the overview data
+        if (field in overviewData) {
+            // Create a new list item
+            let listItem = document.createElement('li');
+
+            // Format the key and value
+            let formattedKey = addSpaces(field);
+            let formattedValue = isNaN(overviewData[field]) ? overviewData[field] : numberWithCommas(overviewData[field]);
+
+            // Set the text of the list item to the formatted key and value
+            listItem.innerHTML = `<span class="key">${formattedKey}:</span> <span class="value">${formattedValue}</span>`;
+
+            // Add the list item to the category container
+            categoryContainer.appendChild(listItem);
+        }
+    }
+
+    // Add the category container to the sidebar
+    sidebar.appendChild(categoryContainer);
 }
+
