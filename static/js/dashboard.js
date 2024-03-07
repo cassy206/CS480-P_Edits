4438620ae4d39d6b3bddba5dfb9c25fdf1d110d4
cassy206 <84590186+cassy206@users.noreply.github.com>
2024-03-06 16:34:57 -0800

    Update dashboard.js
    removing characters teacher mentioned

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -25,22 +25,22 @@ for (let symbol in stockData) {
 
    let high = document.createElement("h3");
    high.className = "high";
-   high.textContent = "High: $" + parseFloat(recentData['2. high']).toFixed(2);
+   high.textContent = "H $" + parseFloat(recentData['2. high']).toFixed(2);
    div.appendChild(high);
 
    let low = document.createElement("h3");
    low.className = "low";
-   low.textContent = "Low: $" + parseFloat(recentData['3. low']).toFixed(2);
+   low.textContent = "L $" + parseFloat(recentData['3. low']).toFixed(2);
    div.appendChild(low);
 
    let open = document.createElement("h3");
    open.className = "open";
-   open.textContent = "Open: $" + parseFloat(recentData['1. open']).toFixed(2);
+   open.textContent = "O $" + parseFloat(recentData['1. open']).toFixed(2);
    div.appendChild(open);
 
    let close = document.createElement("h3");
    close.className = "close";
-   close.textContent = "Close: $" + parseFloat(recentData['4. close']).toFixed(2);
+   close.textContent = "C $" + parseFloat(recentData['4. close']).toFixed(2);
    div.appendChild(close);
 
    // Add the onclick event
